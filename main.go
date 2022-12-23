9dbaf22064e01a40c87ee17cfec7d7c2bf0ed9f7
Vlad <juzopigeon@gmail.com>
2022-12-23 08:37:56 +0600

    Create main.go

---
+++ main.go
@@ -0,0 +1,21 @@
+package main 
+
+import "./database"
+import "./routes"
+import "github.com/gofiber/fiber/v2"
+import "github.com/gofiber/fiber/middleware/cors"
+
+func main() {
+
+	database.Connect()
+
+	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowCredentials: true,
+	}))
+
+ routes.Setup(app)
+
+	app.Listen(":8000")
+}
